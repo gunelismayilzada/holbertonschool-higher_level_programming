1d0f9a579d91dd460fb40f4ef865811df9ac271e
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-11-01 20:28:55 +0000

    task

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# This program prints a welcome message using string concatenation
+# print a welcome message using str1 and str2
 str1 = "Welcome to "
 str2 = "Holberton School!"
 print(str1 + str2)
