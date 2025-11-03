df7c6f3b45a993fde7eb6d81c693ee2ca61bdda3
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-11-03 19:41:41 +0000

    task if else

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# 5-print_comb2.py
-
-for i in range(0, 100):
+for i in range(100):
     if i != 99:
-        print("{:02d}".format(i), end=", ")
+        print("{:02d}".format(i), end=', ')
     else:
-        print("{:02d}".format(i))
+        print("{}".format(i))
