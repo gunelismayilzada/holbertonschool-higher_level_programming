0f4307061e2e4c9534e76a12fbbdfdfe4f0e19cc
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-11-03 18:56:26 +0000

    task

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = "programming"
-str3 = "with"
-str4 = "Python"
-print(str1 + " " + str2 + " " + str3 + " " + str4)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = "object-oriented programming with Python"
+print(str)
