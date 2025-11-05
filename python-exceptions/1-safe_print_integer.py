f1213e6a789924d3c74a2a3b274a5eca10061990
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-11-05 16:43:32 +0400

    task

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception:
+    except (ValueError, TypeError):
         return False
 
+    
