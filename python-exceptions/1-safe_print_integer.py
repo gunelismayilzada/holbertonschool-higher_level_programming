575f5e39b45d4fe7a8dfb49b82849bfc72fc4b01
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-11-05 16:27:34 +0400

    task

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """Print an integer using '{:d}'.format() and return True if successful, False otherwise."""
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
+
