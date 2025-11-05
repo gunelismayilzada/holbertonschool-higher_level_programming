f47a81932124c4b48f0885d336dffb38d5bb7470
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-11-05 17:00:00 +0400

    task

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """Print the first x elements of a list if they are integers."""
+    count = 0
+    i = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass  # skip non-integers silently
+        except IndexError:
+            break  # stop if index out of range
+        i += 1
+    print()
+    return count
