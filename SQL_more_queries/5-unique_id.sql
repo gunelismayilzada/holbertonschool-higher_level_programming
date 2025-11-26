481755d4fb226fc3c2c18c579181f599319afabe
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-11-26 18:01:36 +0400

    sql

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- 5-unique_id.sql
+-- Create table unique_id with id (default 1, unique) and name columns
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
