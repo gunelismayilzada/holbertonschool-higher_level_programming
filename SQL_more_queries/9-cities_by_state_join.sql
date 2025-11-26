96b88915e3c5dd08effe19e4d6eddc5e59719328
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-11-26 18:07:21 +0400

    sql

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- 9-cities_by_state_join.sql
+-- List all cities with their state name using a JOIN
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
