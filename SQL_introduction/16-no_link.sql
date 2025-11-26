af0b87832e5e10575ed3c2b3c54821f0ecde4576
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-11-26 15:06:28 +0400

    sql

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,1 +1,5 @@
-
+-- List all records of second_table where name is not NULL, showing score and name, ordered by descending score
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
