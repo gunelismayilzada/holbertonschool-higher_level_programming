8a9cb1a8eff09a065ee4fce89ac20d2f4ff54751
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-11-30 15:27:09 +0400

    sql

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- 100-not_my_genres.sql
+-- List all genres not linked to the show Dexter
+SELECT name
+FROM tv_genres
+WHERE id NOT IN (
+    SELECT genre_id
+    FROM tv_show_genres
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY name ASC;
