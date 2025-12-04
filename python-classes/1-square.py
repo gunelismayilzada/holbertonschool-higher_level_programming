24375a326d893665751531d121a7b758e0ffb08a
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-12-04 20:30:40 +0000

    classes

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-"""Defines an empty class Square."""
+"""Module that defines a Square class with a private size attribute."""
 
 
 class Square:
-    """This is an empty class that defines a square."""
-    pass
+    """Represents a square.
+    
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+    
+    def __init__(self, size):
+        """Initialize a new Square instance.
+        
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
